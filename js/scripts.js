d845cdada535a7e493b8d1a4ab16ace6e8d52fb7
Yelena <ynedelko@gmail.com>
2015-09-11 11:03:41 -0700

    Refactor code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
 
 var findReplace = function(string, word, replaceWith) {
-  var newString = "";
-  newString = string.replace(new RegExp(word, 'g'), replaceWith);
-  return newString;
+  return string.replace(new RegExp(word, 'g'), replaceWith);
 };

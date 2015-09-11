0eb0170a8f4dc48621ab93d8304214d848f9e7ca
Yelena <ynedelko@gmail.com>
2015-09-11 11:55:28 -0700

    Add web views.

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,18 @@
 var findReplace = function(string, word, replaceWith) {
   return string.replace(new RegExp(word, 'g'), replaceWith);
 };
+
+$(document).ready(function() {
+  $("form#find-and-replace").submit(function(event) {
+    var string = ($('input#string').val());
+    var word = ($('input#word').val());
+    var replaceWith = ($('input#replaceWith').val());
+    var output = findReplace(string, word, replaceWith);
+
+    $(".input").text(string);
+    $(".output").text(output);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
